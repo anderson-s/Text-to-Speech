01874ab569bd39e49b7c74d72cfe530d6cc541cc
Andeson Silva <andsilva607@gmail.com>
2022-10-04 14:30:14 -0300

    Inserindo projeto

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Text to speech'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final FlutterTts flutterTts = FlutterTts();
+  final TextEditingController _controller = TextEditingController();
+
+  _reproduzir() async{
+    await flutterTts.setLanguage("pt-BR");
+    await flutterTts.setPitch(1);
+    await flutterTts.speak(_controller.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _controller,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _reproduzir();
+              },
+              child: const Text("Converter"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
